7bde64adb120e6b52891602b6fcdb31571319801
Radoslaw Lisowski <radlisowski@gmail.com>
2025-05-19 00:52:31 +0100

    added more tests for the about page

---
+++ page_object_model/about_page.js
@@ -0,0 +1,52 @@
+const {expect} = require('@playwright/test');
+
+class AboutPage {
+    constructor(page) {
+        this.page = page;
+        this.blogButton = page.locator(`nav.navbar a:has-text("blog")`);
+        this.projectsButton = page.locator(`nav.navbar a:has-text("projects")`);
+        this.aboutButton = page.locator(`nav.navbar a:has-text("about")`);
+        this.homeButton = page.locator(`nav.navbar a:has-text("home")`);
+        this.githubButton = page.locator(`nav.footer a:has-text("github")`);
+        this.linkedinButton = page.locator(`nav.footer a:has-text("linkedin")`);
+        this.logo = page.locator(`.hero-box`);
+        this.profileImage = page.locator('.profile-pic');
+        this.headingText = page.locator('.about h5');
+        this.blogLink = page.locator('.about a[href="/blog.html"]');
+        this.buttons = [
+            this.homeButton,
+            this.blogButton,
+            this.projectsButton,
+            this.aboutButton,
+            this.githubButton,
+            this.linkedinButton
+        ];
+    }
+
+    async navigate() {
+        await this.page.goto('https://radlisowski.github.io/about.html');
+    }
+
+    async validateNavigationButtons() {
+        for (const button of this.buttons) {
+            await expect(button).toBeVisible();
+        }
+    }
+
+    async validateProfileImage() {
+        await expect(this.profileImage).toBeVisible();
+        await expect(this.profileImage).toHaveAttribute('src', '/images/profile.jpg');
+    }
+    async validateHeadingText() {
+        await expect(this.headingText).toBeVisible();
+        await expect(this.headingText).toHaveText('My name is Radoslaw Lisowski, but people call me Rado...');
+    }
+    async validateBlogLink() {
+        await expect(this.blogLink).toBeVisible();
+        await expect(this.blogLink).toHaveAttribute('href', '/blog.html');
+    }
+    
+    
+}
+
+module.exports = {AboutPage};
--- tests/portfolio_page_tests.spec.js
+++ tests/portfolio_page_tests.spec.js
@@ -3,22 +3,33 @@ import {test} from '@playwright/test';
 import {HomePage} from '../page_object_model/home_page';
 import {ProjectsPage} from '../page_object_model/projects_page';
 import {BlogPage} from '../page_object_model/blog_page';
+import {AboutPage} from "../page_object_model/about_page";
 
 test.describe('Validate all navigation links are on all the pages and are visible', () => {
 
-    test('Validate the objects on Landing page', async ({page}) => {
+    test('Validate the navigation on Landing page', async ({page}) => {
         const homePage = new HomePage(page);
         await homePage.navigate();
         await homePage.validatePageTitle();
         await homePage.validateNavigationButtons();
     });
 
-    test('Validate the objects on Projects page', async ({page}) => {
+    test('Validate the navigation on Projects page', async ({page}) => {
         const projectsPage = new ProjectsPage(page);
         await projectsPage.navigate();
         await projectsPage.validatePageTitle();
         await projectsPage.validateNavigationButtons();
     });
+    test('Validate the navigation on Blog page', async ({page}) => {
+        const blogPage = new BlogPage(page);
+        await blogPage.navigate();
+        await blogPage.validateNavigationButtons();
+    });
+    test('Validate the navigation on About page', async ({page}) => {
+        const aboutPage = new AboutPage(page);
+        await aboutPage.navigate();
+        await aboutPage.validateNavigationButtons();
+    })
 });
 
 
@@ -70,5 +81,24 @@ test.describe('Blog page validation', () => {
 });
 
 
+test.describe('About page validation', () => {
 
+    test('Validate profile image', async ({page}) => {
+        const aboutPage = new AboutPage(page);
+        await aboutPage.navigate();
+        await aboutPage.validateProfileImage();
+    });
+
+    test('Validate heading text', async ({page}) => {
+        const aboutPage = new AboutPage(page);
+        await aboutPage.navigate();
+        await aboutPage.validateHeadingText();
+    });
+
+    test('Validate blog link', async ({page}) => {
+        const aboutPage = new AboutPage(page);
+        await aboutPage.navigate();
+        await aboutPage.validateBlogLink();
+    });
+});
 
