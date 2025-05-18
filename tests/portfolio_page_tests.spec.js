62fffbd020e3e2dfb4bce071e4165bdc784f50f9
Radoslaw Lisowski <radlisowski@gmail.com>
2025-05-19 00:15:23 +0100

    added more tests for project page and home page

--- tests/portfolio_page_tests.spec.js
+++ tests/portfolio_page_tests.spec.js
@@ -1,37 +1,39 @@
 // @ts-check
-import { test } from '@playwright/test';
+import { test} from '@playwright/test';
 import {HomePage} from '../page_object_model/home_page';
+import {ProjectsPage} from '../page_object_model/projects_page';
 
-// async function validateNavButton(buttonText, expectedUrl) {
-//   const button = page.locator(`nav.navbar a:has-text("|${buttonText}|")`);
-//   await expect(button).toBeVisible();
-//   await expect(button).toBeEnabled();
-//   await button.click();
-//   await expect(page).toHaveURL(expectedUrl);
-// }
+test.describe('Validate all navigation links are on the pages and are visible', () => {
+    test('Validate the objects on Landing page', async ({page}) => {
+        const homePage = new HomePage(page);
+        await homePage.navigate();
+        await homePage.validatePageTitle();
+        await homePage.validateNavigationButtons();
+    });
 
-test.describe('Validate all headers are on the page and visible', () => {
-  test('Validate the objects on Landing page', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.navigate();
-    //await homePage.validatePageTitle();
-    await homePage.validateNavigationButtons();
-  });
+    test('Validate the objects on Projects page', async ({page}) => {
+        const projectsPage = new ProjectsPage(page);
+        await projectsPage.navigate();
+        await projectsPage.validatePageTitle();
+        await projectsPage.validateNavigationButtons();
+    });
+});
 
-  // test('Validate the objects on Projects page', async ({page}) => {
-  //   await page.goto('https://radlisowski.github.io/');
-  //   await validateNavButton(page, 'projects', 'https://radlisowski.github.io/projects.html');
-  // });
-  //
-  // test('Validate the objects on Blog page', async ({page}) => {
-  //   await page.goto('https://radlisowski.github.io/');
-  //   await validateNavButton(page, 'blog', 'https://radlisowski.github.io/blog.html');
-  // });
-  //
-  // test('Validate the objects on About page', async ({page}) => {
-  //   await page.goto('https://radlisowski.github.io/');
-  //   await validateNavButton(page, 'about', 'https://radlisowski.github.io/about.html');
-  // });
+test.describe('Validate all objects on the pages are visible', () => {
+    test('Validate the objects on home page', async ({page}) => {
+        const homePage = new HomePage(page);
+        await homePage.navigate();
+        await homePage.validateLogo()
+    });
 
-})
+    test('Validate project cards count and titles on the project page', async ({page}) => {
+        const projectsPage = new ProjectsPage(page);
+        await projectsPage.navigate();
+        await projectsPage.validateProjectCardsNumbersAndTitles();
+
+    });
+});
+
+
+
 
